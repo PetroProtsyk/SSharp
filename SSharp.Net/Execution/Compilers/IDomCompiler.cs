18cebd7c9da217e7fb7edd011f834b530dbcce1b
Petro <piter.protsyk@gmail.com>
2011-01-12 16:05:49 +0200

    Applied Apache 2.0 license to source code

--- SSharp.Net/Execution/Compilers/IDomCompiler.cs
+++ SSharp.Net/Execution/Compilers/IDomCompiler.cs
@@ -1,7 +1,23 @@
-﻿namespace Scripting.SSharp.Execution.Compilers
+﻿/*
+ * Copyright © 2011, Petro Protsyk, Denys Vuika
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Scripting.SSharp.Execution.Compilers
 {
   using Dom;
-  using Scripting.SSharp.Parser.Ast;
+  using Parser.Ast;
 
   internal interface IDomCompiler : ICompiler<AstNode, CodeProgram, CodeObject>
   {
