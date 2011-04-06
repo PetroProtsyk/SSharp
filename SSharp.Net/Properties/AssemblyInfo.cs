c825c96b4edc931bfb740cfd4c2c2035b6d56a71
Petro <piter.protsyk@gmail.com>
2011-04-06 10:02:49 +0200

    Development Branch

--- SSharp.Net/Properties/AssemblyInfo.cs
+++ SSharp.Net/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.1.*")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
 
 [assembly: InternalsVisibleTo("Scripting.SSharp.UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001008df4fedfcc46bb2ffb5f88af822bfd4c6230c3d4200d5e19f1e09d181f55f24fb5b491d1338fb09268dd5bdc21c165665379f9c7771000d7a3b9c1cbabc122a02dcb94d6a424ed9e18516ffc76fb9d6f6acaba3740178721cd2d2a3a4b393fcfa5a467029828952e7322147d19a3b80c89ddc5c6712300ce8a9835675a72a7d1")]
