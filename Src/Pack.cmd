c0ab6e585ff0d42e22349066b5217ba7138cceee
petro <petro@example.com>
2021-03-16 20:03:31 +0100

    NuGet package configuration

--- Src/Pack.cmd
+++ Src/Pack.cmd
@@ -1,5 +1,4 @@
-dotnet build SSharp.Net -c=Release /p:Platform="Any CPU" /p:TargetFramework="netcoreapp3.1" --no-incremental
-dotnet build SSharp.Net -c=Release_NET4 /p:Platform="Any CPU" /p:TargetFramework="net48" --no-incremental
-dotnet pack SSharp.Net -c=Release_NET4 --no-build -o=./
+dotnet build SSharp.Net -c=Release
+dotnet pack SSharp.Net -c=Release --no-build -o=./
 
-powershell "Get-FileHash .\SSharp.Net.4.0.1.nupkg  -Algorithm SHA512 | Format-List"
+powershell "Get-FileHash .\SSharp.Net.4.1.1.nupkg  -Algorithm SHA512 | Format-List"
